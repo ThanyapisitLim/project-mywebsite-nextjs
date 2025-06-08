21ddd0c919ac5aa283aed0476ed75e6adcfc7f46
Thanyapisit Limpakanon <152508112+ThanyapisitLim@users.noreply.github.com>
2025-06-08 16:46:45 +0700

    home img

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
 
 
 export const metadata: Metadata = {
@@ -15,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,30 @@
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
+/* eslint-disable react/no-unescaped-entities */
+import Image from "next/image";
 
 export default function Page() {
   return (
-    <main>
-      <Navbar/>
+    <main className="relative w-full h-screen overflow-hidden">
+      {/* Background Image */}
+      <Image
+        src="https://images.unsplash.com/photo-1728290838813-9c94d553d988?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="Background image"
+        fill
+        priority
+        className="object-cover"
+      />
 
-      <Footer/>
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-black/40 z-0" />
+
+      {/* Content */}
+      <div className="absolute bottom-10 left-10 z-10 text-white space-y-4 bg-black p-10 rounded-2xl">
+        <h1 className="text-5xl md:text-6xl font-bold">Hello, I'm Thanyapisit</h1>
+        <p className="text-lg md:text-xl">
+          Web developer passionate about building beautiful and functional apps.
+        </p>
+      </div>
+
+      
     </main>
   );
 }
